db93edadf6aa94ad58396afa0c0d4afc2a19edbf
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-21 19:46:02 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -5,7 +5,7 @@ prompt.get("input", function (err, result) {
   let input = result.input;
   let numbers = input.split(",");
   let a = parseInt(numbers[0]);
-  let a = parseInt(numbers[1]);
+  let b = parseInt(numbers[1]);
   let sum=0;
   for (i = a; i <= b; i++) {
   
