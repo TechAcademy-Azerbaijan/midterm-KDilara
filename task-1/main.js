e05e863135daaca2d65310e2d9a5116bacb7ac50
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-21 19:11:24 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,16 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+
+    let input = result.input; 
+    let numbers = input.split(','); 
+    let a = parseInt(numbers[0]); 
+    let sum = 0;
+    while (a > 0) {
+      num = a % 10;
+      sum = sum + num;
+      a = parseInt(a / 10);
+    }
+    console.log(sum);
 });
