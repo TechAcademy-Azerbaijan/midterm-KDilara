c4f794a81b347909be0b9612caff3e1a6c5ae1d7
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-21 19:29:59 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,19 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+
+    let input = result.input; 
+    let numbers = input.split(','); 
+    let a = parseInt(numbers[0]); 
+    a1=a%10;
+    a2=parseInt(a/10)%10;
+    a3=parseInt(a/100)%10;
+    a4=parseInt(a/1000)%10;
+    a5=parseInt(a/10000);
+    if(a1===a2 || a2===a3 || a3===a4 || a4===a5 || a1===a3 ||a1===a4 ||a1===a5 || a2===a4 ||a2===a5|| a3===a5){
+      console.log("YES")
+    }else{
+      console.log("NO")
+    }
 });
