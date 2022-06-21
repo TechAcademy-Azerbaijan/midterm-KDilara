bd0d1283e3cf68a712ec871c870bcb40920e21f2
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-21 19:44:55 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,18 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
+prompt.get("input", function (err, result) {
+  let input = result.input;
+  let numbers = input.split(",");
+  let a = parseInt(numbers[0]);
+  let a = parseInt(numbers[1]);
+  let sum=0;
+  for (i = a; i <= b; i++) {
   
-    // Write code here
-  
+        if(i%2!==0){
+          sum=sum+i;
+        }
+      
+    }
+    console.log(sum)
 });
