95c50fe4664c8348ff3a155af7d81ce719d21b9e
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-21 19:41:53 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,18 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  let input = result.input;
+  let numbers = input.split(",");
+  let a = parseInt(numbers[0]);
+  let i;
+for (i = 1; i <= a; i++) {
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  if (a % i === 0) {
+   
+      if(i%2!==0){
+        console.log(i)
+      }
+    }
+  }
 });
